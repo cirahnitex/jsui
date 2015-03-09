07511c855e789f0a0ea6bcfd3ac6531f9ea26e93
CirahYan <cirahnitex@gmail.com>
2015-03-09 17:51:51 +0800

    Select Widget

--- Root/Widget/Inputable/Select.View.js
+++ Root/Widget/Inputable/Select.View.js
@@ -15,18 +15,31 @@
             that._ctrl.focus();
         });
 
-        // blur the widget when the input dom is blurred, with a short delay.
+        // commit the value and blur the widget when the input dom is blurred, with a short delay.
         // because we need a short amount of time to click the option.
         this.eles.input.addEventListener('blur', function() {
+            that._commitValue();
             setTimeout(function() {
                 that._ctrl.blur();
             }, 100);
         });
 
         // give some hint when typing into the input
-        this.eles.input.addEventListener('keyup', function() {
+        this.eles.input.addEventListener('keyup', function(e) {
+            switch(e.which) {
+            case 38:
+                that._hoverPreviousOption();
+                break;
+            case 40:
+                that._hoverNextOption();
+                break;
+            case 13:
+                that._selectHoveredOption();
+                break;
+            }
             var searchResult = ctrl.searchOption(this.value);
             that._displaySearchedOptions(searchResult);
+            that._hoverFirstOption();
         });
 
         // list all options
@@ -41,9 +54,14 @@
             var optionDom = this.createWidget(this.eles.optionWrap,'option',option.getDisplay());
             optionDom[_option] = option;
 
+            // mouseover the dom to hover
+            optionDom.onmouseover = function() {
+                that._hoverOption(this);
+            };
+
             // click the dom to select the option
             optionDom.onmousedown = function() {
-                that._ctrl.setValue(this[_option].value);
+                that._selectHoveredOption();
             };
         }
     };
@@ -53,6 +71,7 @@
         switch(state) {
         case ns.state.FOCUSED:
             this.eles.wrap.classList.add('focused');
+            this._hoverFirstOption();
             break;
         case ns.state.BLURRED:
             this.eles.wrap.classList.remove('focused');
@@ -110,6 +129,9 @@
             }
         }
     };
+    ns.View.prototype._hoverFirstOption = function() {
+        this._hoverOption(this.querySelector('.option:not(.hide)'));
+    };
     /**
      * hover previous option
      * @private
@@ -118,11 +140,17 @@
         // select the hovered option
         var dom = this.querySelector('.option.hover');
 
-        // hover its previous sibling
-        var sib = dom.previousSibling;
+        // if exists, hover its previous sibling
+        if(dom) {
+            var sib = dom.previousSibling;
 
-        // if its an element, hover it
-        if(sib instanceof HTMLElement) this._hoverOption(sib);
+            // if its an element, hover it
+            if(sib instanceof HTMLElement) this._hoverOption(sib);
+            return;
+        }
+        // if not, hover the first option
+        var dom = this.querySelector('.option');
+        this._hoverOption(dom);
     };
     /**
      *
@@ -132,10 +160,57 @@
         // select the hovered option
         var dom = this.querySelector('.option.hover');
 
-        // hover its next sibling
-        var sib = dom.nextSibling;
+        // if exists, hover its next sibling
+        if(dom) {
+            var sib = dom.nextSibling;
 
-        // if its an element, hover it
-        if(sib instanceof HTMLElement) this._hoverOption(sib);
+            // if its an element, hover it
+            if(sib instanceof HTMLElement) this._hoverOption(sib);
+            return;
+        }
+        // if not, hover the first option
+        var dom = this.querySelector('.option');
+        this._hoverOption(dom);
+
+    };
+    ns.View.prototype._selectHoveredOption = function() {
+        // select the hovered option
+        var option = this.querySelector('.option.hover');
+
+        // do nothing if no option is hovered
+        if(!option) return;
+
+        // get its display string
+        option = option[ns.View.OPTION_DOM_OPTION_VARNAME];
+        // fill into the input
+        //noinspection JSPrimitiveTypeWrapperUsage
+        this.eles.input.value = option.getDisplay();
+
+        // commit value
+        this._ctrl.setValue(option.value);
+
+    };
+    ns.View.prototype._commitValue = function() {
+        // get content in input
+        var display = this.eles.input.value;
+
+        // list all options
+        var aOption = this._ctrl.listOption();
+
+        for(var i=0; i<aOption.length; i++) {
+            var option = aOption[i];
+
+            // find the option that match the input content
+            // commit that value
+            if(option.getDisplay() === display) {
+                this._ctrl.setValue(option.value);
+                return;
+            }
+
+        }
+
+        // commit the content if no matching option found
+        this._ctrl.setValue(display);
+
     };
 })();
